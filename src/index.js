b9913c982cbc2ec1a269e50a4db0696cb5b4f144
ChristianJaziel <christianjaziel.nicolasrayon2@gmail.com>
2023-03-18 14:13:41 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+//inicializaciones
+const app = express();
+
+
+//configuraciones
+app.set('port', process.env.PORT || 3550);  
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, app.get('views'), 'layouts'),
+     extname: '.hbs',
+     helpers: require('./lib/handlebars.js')
+}));
+app.set('view engine', 'hbs');
+
+//Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Variables globales
+app.use((req, res, next)=>{
+
+    next();
+});
+
+//Rutas
+app.use(require('./routes/index.js'));
+
+//Publico
+
+
+//Corriendo servidor
+app.listen(app.get('port'), ()=>{
+    console.log('Servidor inicializado en el puerto ', app.get('port'));
+})
