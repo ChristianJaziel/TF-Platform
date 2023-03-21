c36ca62e9c89c369aa7bfb09871a8b57fadc37a2
ChristianJaziel <christianjaziel.nicolasrayon2@gmail.com>
2023-03-20 18:27:49 -0600

    Insersión de datos en la bd

--- src/routes/registropersonas.js
+++ src/routes/registropersonas.js
@@ -10,7 +10,18 @@ router.get('/add',(req, res)=>{
     res.render('personas/add');
 });
 
-router.post('/add',(req, res)=>{
+router.post('/add',async (req, res)=>{
+    const { nombres_persona, a_paterno, a_materno, calle, numero_casa, colonia, num_tel} = req.body;
+    const newRegistro = {
+        nombres_persona, 
+        a_paterno, 
+        a_materno, 
+        calle, 
+        numero_casa, 
+        colonia, 
+        num_tel
+    };
+    await db.query('INSERT INTO personas SET ?', [newRegistro]);
     res.send("Recibido");
 });
 
