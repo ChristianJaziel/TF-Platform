0b7b22c7f1bc3ae4ee1fe0673e4f8031b2aa2e7f
ChristianJaziel <christianjaziel.nicolasrayon2@gmail.com>
2023-04-15 00:53:06 -0600

    Siembras post also complete

--- src/routes/siembras.js
+++ src/routes/siembras.js
@@ -10,7 +10,36 @@ router.get('/add',(req, res)=>{
 });
 
 router.post('/add',async(req, res)=>{
-    
+    const {
+        nom_productoSi,
+        cantidad,
+        descripcion_cant,
+        genero_producto,
+        id_personaSi,
+        nombresSi,
+        a_paterno,
+        a_materno,
+        num_tel,
+        nom_recibe,
+        fecha_recibe
+    } = req.body;
+    const newRegistro = {
+        nom_productoSi,
+        cantidad,
+        descripcion_cant,
+        genero_producto,
+        id_personaSi,
+        nombresSi,
+        a_paterno,
+        a_materno,
+        num_tel,
+        nom_recibe,
+        fecha_recibe
+    };
+    console.log(newRegistro);
+    res.send("Enviado");
+    //await db.query('INSERT INTO siembra SET ?', [newRegistro]);
+
 });
 
 module.exports = router;
